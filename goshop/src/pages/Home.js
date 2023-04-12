25b39f2ef07d7643300d341f950e8e406fe7c1e6
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-04-12 17:34:34 +0100

    Product Card section of the homepage done

--- goshop/src/pages/Home.js
+++ goshop/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom"
 import Marquee from "react-fast-marquee";
 import BlogCard from '../components/BlogCard';
+import ProductCard from '../components/ProductCard';
 
 
 
@@ -187,6 +188,20 @@ const Home = () => {
                 </div>
             </section>
 
+            <section className="featured-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">Featured Collections</h3>
+                        </div>
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                    </div>
+                </div>
+            </section>
+
             <section className="marque-wrapper py-5">
                 <div className="container-xxl">
                     <div className="row">
@@ -238,7 +253,6 @@ const Home = () => {
                 </div>
             </section>
 
-            
         </>
     )
 };

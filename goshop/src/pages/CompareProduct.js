2cb1a43ceb47465a67f69af32d7e252a72d0c5d2
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-04-15 16:09:01 +0100

    compare product page done

---
+++ goshop/src/pages/CompareProduct.js
@@ -0,0 +1,225 @@
+import React from 'react';
+import BreadCrumb from '../components/BreadCrumb';
+import Meta from '../components/Meta';
+import Color from '../components/Color';
+
+const CompareProduct = () => {
+    return (
+        <>
+            <Meta title={"Compare Products"}/>
+            <BreadCrumb title="Compare Products" /> 
+            <div className="compare-product-wrapper py-5 home-wrapper-2">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                                <div className="product-card-image">
+                                    <img src="images/watch.jpg" alt="watch" />
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className='title'>Honor T1 7.0 1GB RAM 8 GB ROM 7inch Width w1-Fi+3G Tablet</h5>
+                                    <h6 className="price mb-3 mt-3">$ 100</h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Brand:</h3>
+                                            <p>Havels</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Type:</h3>
+                                            <p>Watch</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Availability:</h3>
+                                            <p>In Store</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Color:</h3>
+                                            <Color/>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Size:</h3>
+                                            <div className='d-flex gap-10'>
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                                <div className="product-card-image">
+                                    <img src="images/watch.jpg" alt="watch" />
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className='title'>Honor T1 7.0 1GB RAM 8 GB ROM 7inch Width w1-Fi+3G Tablet</h5>
+                                    <h6 className="price mb-3 mt-3">$ 100</h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Brand:</h3>
+                                            <p>Havels</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Type:</h3>
+                                            <p>Watch</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Availability:</h3>
+                                            <p>In Store</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Color:</h3>
+                                            <Color/>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Size:</h3>
+                                            <div className='d-flex gap-10'>
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                                <div className="product-card-image">
+                                    <img src="images/watch.jpg" alt="watch" />
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className='title'>Honor T1 7.0 1GB RAM 8 GB ROM 7inch Width w1-Fi+3G Tablet</h5>
+                                    <h6 className="price mb-3 mt-3">$ 100</h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Brand:</h3>
+                                            <p>Havels</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Type:</h3>
+                                            <p>Watch</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Availability:</h3>
+                                            <p>In Store</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Color:</h3>
+                                            <Color/>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Size:</h3>
+                                            <div className='d-flex gap-10'>
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div className="col-3">
+                            <div className="compare-product-card position-relative">
+                                <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                                <div className="product-card-image">
+                                    <img src="images/watch.jpg" alt="watch" />
+                                </div>
+                                <div className="compare-product-details">
+                                    <h5 className='title'>Honor T1 7.0 1GB RAM 8 GB ROM 7inch Width w1-Fi+3G Tablet</h5>
+                                    <h6 className="price mb-3 mt-3">$ 100</h6>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Brand:</h3>
+                                            <p>Havels</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Type:</h3>
+                                            <p>Watch</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Availability:</h3>
+                                            <p>In Store</p>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Color:</h3>
+                                            <Color/>
+                                        </div>
+                                    </div>
+
+                                    <div>
+                                        <div className="product-detail">
+                                            <h3>Size:</h3>
+                                            <div className='d-flex gap-10'>
+                                                <p>S</p>
+                                                <p>M</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default CompareProduct;

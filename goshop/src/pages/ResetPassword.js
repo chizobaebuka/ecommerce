2527c9359efe02cfb339aa31ec0c2eee7dbbd9f8
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-04-17 23:00:31 +0100

    pages done

--- goshop/src/pages/ResetPassword.js
+++ goshop/src/pages/ResetPassword.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import BreadCrumb from '../components/BreadCrumb';
 import Meta from '../components/Meta';
-import { Link } from 'react-router-dom';
 
-const Login = () => {
+const ResetPassword = () => {
     return (
         <>
-            <Meta title={"Login"}/>
-            <BreadCrumb title="Login" /> 
+            <Meta title={"Reset Password"}/>
+            <BreadCrumb title="Reset Password" />
 
             <div className="login-wrapper home-wrapper-2 py-5">
-                <div className="container-xxl">
-                    <div className="row">
+                <div className="row">
+                    <div className="container-xxl">
                         <div className="col-12">
                             <div className="auth-card">
-                                <h3 className='text-center mb-3'>Login</h3>
+                                <h3 className='text-center mb-3'>Reset Password</h3>
                                 <form action="" className='d-flex flex-column gap-15'>
                                     <div>
-                                        <input type="email" name='email' placeholder='Email' className="form-control" />
-                                    </div>
-                                    <div className='mt-3'>
                                         <input type="password" name='password' placeholder='Password' className="form-control" />
                                     </div>
+                                    <div className='mt-3'>
+                                        <input type="password" name='confpassword' placeholder='Confirm Password' className="form-control" />
+                                    </div>
                                     <div>
-                                        <Link to="/forgot-password">Forgot Password</Link>
                                         <div className='mt-3 d-flex justify-content-center gap-15 align-items-center'>
-                                            <button className='button border-0' type='submit'>Login</button>
-                                            <Link to="/signup" className='button signup'>Sign Up</Link>
+                                            <button className='button border-0'>Continue</button>
                                         </div>
                                     </div>
                                 </form>
@@ -39,4 +36,4 @@ const Login = () => {
     );
 };
 
-export default Login
+export default ResetPassword;

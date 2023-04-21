5993594e1019bd65ad258c7a4f29f4e14fc57f3a
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-04-21 16:57:28 +0100

    added routes for some pages

--- goshop/src/App.js
+++ goshop/src/App.js
@@ -9,6 +9,16 @@ import OurStore from './pages/OurStore';
 import Blog from './pages/Blog';
 import CompareProduct from './pages/CompareProduct';
 import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import Signup from './pages/Signup';
+import ResetPassword from './pages/ResetPassword';
+import SingleBlog from './pages/SingleBlog';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+import ShippingPolicy from './pages/ShippingPolicy';
+import RefundPolicy from './pages/RefundPolicy';
+import TermsAndConditions from './pages/TermsandConditions';
+import SingleProduct from './pages/SingleProduct';
 
 function App() {
   return (
@@ -19,10 +29,20 @@ function App() {
             <Route index element={<Home/>}/>
             <Route path='about' element={<About/>}/>
             <Route path='contact' element={<Contact/>}/>
-            <Route path='store' element={<OurStore/>}/>
+            <Route path='product' element={<OurStore/>}/>
+            <Route path='product/:id' element={<SingleProduct/>}/>
             <Route path='blogs' element={<Blog/>}/>
             <Route path='compare-product' element={<CompareProduct/>}/>
             <Route path='wishlist' element={<Wishlist/>}/>
+            <Route path='login' element={<Login/>}/>
+            <Route path='forgot-password' element={<ForgotPassword/>}/>
+            <Route path='signup' element={<Signup/>}/>
+            <Route path='reset-password' element={<ResetPassword/>}/>
+            <Route path='blog-id' element={<SingleBlog/>}/>
+            <Route path='privacy-policy' element={<PrivacyPolicy/>}/>
+            <Route path='shipping-policy' element={<ShippingPolicy/>}/>
+            <Route path='refund-policy' element={<RefundPolicy/>}/>
+            <Route path='term-conditions' element={<TermsAndConditions/>}/>
           </Route>
         </Routes>
       </BrowserRouter>

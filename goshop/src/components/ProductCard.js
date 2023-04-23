8abad2bd285e88b44c6b579fdefdc6dbddb3ceaf
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2023-04-23 21:40:45 +0100

    updated

--- goshop/src/components/ProductCard.js
+++ goshop/src/components/ProductCard.js
@@ -16,7 +16,7 @@ const ProductCard = (props) => {
     return (
         <>
             <div className={` ${location.pathname === "/product" ? `gr-${grid}` : "col-3"} `}>
-                <Link to="/product/:id" className="product-card position-relative">
+                <Link to={`${location.pathname == "/" ? "/product/:id" : location.pathname == "/product/:id"? "/product/:id": ":id"}`} className="product-card position-relative">
                     <div className="wishlist-icon position-absolute">
                         <button className='border-0 bg-transparent'>
                             <img src={wish} alt="wishlist" />
@@ -61,8 +61,8 @@ const ProductCard = (props) => {
                 </Link>
             </div>
 
-            <div className={` ${location.pathname === "/store" ? `gr-${grid}` : "col-3"} `}>
-            <Link className="product-card position-relative">
+            <div className={` ${location.pathname === "/product" ? `gr-${grid}` : "col-3"} `}>
+            <Link to={`${location.pathname == "/" ? "/product/:id" : location.pathname == "/product/:id"? "/product/:id": ":id"}`} className="product-card position-relative">
                 <div className="wishlist-icon position-absolute">
                     <button className='border-0 bg-transparent'>
                         <img src={wish} alt="wishlist" />
